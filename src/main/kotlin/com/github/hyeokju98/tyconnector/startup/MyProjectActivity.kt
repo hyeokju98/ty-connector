4c8c745a4cca310b06a5fc5625c3e0fc27760d1a
GitHub Action <action@github.com>
2025-07-17 08:18:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/hyeokju98/tyconnector/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/hyeokju98/tyconnector/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.hyeokju98.tyconnector.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
